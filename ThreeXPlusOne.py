a7e02fe06cf769074d1519914b7744414c0dae6d
Srajan Dehariya <74010866+Learndev-student@users.noreply.github.com>
2024-01-22 01:21:08 +0530

    Update ThreeXPlusOne.py
    black background and dpi 2000

--- ThreeXPlusOne.py
+++ ThreeXPlusOne.py
@@ -2,13 +2,14 @@
 import matplotlib.pyplot as plt
 import math
 colors = ["red","blue","green","violet","cyan"]
+plt.axes().set_facecolor("black")
 def f(a):
     if a%2==0:
         return a/2
     else:
         return 3*a+1
 
-for i in range(2,1000):
+for i in range(2,500):
         a = i
         x = []
         y = []
@@ -18,5 +19,6 @@ for i in range(2,1000):
             a = f(a)
             x.append(a*math.cos(k/10))
             y.append((a+100)*math.sin(k/10))
-        plt.plot(x,y,color=colors[i%5],linewidth=0.5)
+        plt.plot(x,y,color=colors[i%5],linewidth=0.01)
+plt.savefig("new.png",dpi=2000)
 plt.show()
